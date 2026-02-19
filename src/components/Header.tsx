1bad83e2ee8d0ebfcfc8cd83649702e818704070
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-02-19 17:19:49 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,8 +15,8 @@ function AdaKamiLogo() {
 
 export default function Header() {
   // --- PENGATURAN NOMOR TELEPON ---
-  const displayNumber = '0822-111-378'; // Format tampilan teks
-  const waNumber = '62822111378';      // Format sistem (WA & Link Telpon)
+  const displayNumber = '0821-6364-317'; // Format tampilan teks
+  const waNumber = '628216364317';      // Format sistem (WA & Link Telpon)
   // --------------------------------
 
   return (
