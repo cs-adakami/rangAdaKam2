05f851165d41a288d018f7422a9df5badb5d5eae
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-02-28 10:51:31 +0700

    Update FeatureSection.tsx

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -2,7 +2,7 @@ import { Clock, Shield, Zap, Award } from 'lucide-react';
 
 export default function FeatureSection() {
   // --- UBAH NOMOR WHATSAPP DI SINI ---
-  const waNumber = '62812827822';
+  const waNumber = '628192140072';
   // -----------------------------------
 
   const features = [
