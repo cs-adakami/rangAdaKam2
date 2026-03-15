d8fdc66f3b15998a9c3581120f52036d23a04f0e
Bantuan adakami <144202158+cs-adakami@users.noreply.github.com>
2026-03-15 10:33:16 +0700

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -2,8 +2,8 @@ import { MapPin, Phone, Mail, Clock } from 'lucide-react';
 
 export default function ContactSection() {
   // --- BAGIAN EDIT NOMOR ---
-  const displayPhone = '0822111378'; // Format untuk dilihat user
-  const actionPhone = '62822111378';   // Format untuk sistem (WA & Telpon)
+  const displayPhone = '081918533393'; // Format untuk dilihat user
+  const actionPhone = '6281918533393';   // Format untuk sistem (WA & Telpon)
   // -------------------------
 
   const contactInfo = [
